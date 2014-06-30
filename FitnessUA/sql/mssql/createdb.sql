a9ff7a93aafc48a9a4a81aafc8a3c15c47a82c56
Женя <didevgen@gmail.com>
2014-06-30 21:04:27 +0300

    Написан скрипт создания БД. В файле createdb.sql. Некоторые имена изменены, а именно: "min" на "minimum","max" на "maximum", "user" на "users", "user_id" на "users_id", "date" на "dates"

---
+++ FitnessUA/sql/mssql/createdb.sql
@@ -0,0 +1,11 @@
+create database FitnessUA;
+use FitnessUA;
+create table GroupEnum (id bigint, admin_id bigint, assigned_id bigint);
+create table Users (users_id bigint, login nvarchar, pwdHash nvarchar, email nvarchar, employee_id bigint);
+create table EmployeePref (pref_id bigint, minimum tinyint, maximum tinyint, employee_id bigint);
+create table EmployeeGroups (group_id bigint, title nvarchar);
+create table Employees (employee_id bigint, name nvarchar, sureName nvarchar, threadName nvarchar, group_id bigint);
+create table ClubPrefs (shedule_period_id bigint, club_id bigint, employee_id bigint);
+create table DayShedule (day_shedule_id bigint, dates datetime, halfOfDay tinyint, users_id bigint, club_id bigint, shedule_period_id bigint);
+create table Club (club_id bigint, title nvarchar, isIndependent bit);
+create table ShedulePeriod (shedule_period_id bigint, startDate datetime, endDate datetime, last_period_id bigint);
