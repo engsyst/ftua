2b016cab8a56ac29ff17ff0bb13d1e067a685d14
Volodymyr Semerkov <semerkoff@gmail.com>
2014-07-15 00:22:19 +0300

    Go to GWT project.

--- FitnessUA/src/ua/nure/ostpc/malibu/shedule/Demo.java
+++ FitnessUA/src/ua/nure/ostpc/malibu/shedule/Demo.java
@@ -1,8 +1,48 @@
 package ua.nure.ostpc.malibu.shedule;
 
+import java.sql.Date;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Iterator;
+import java.util.Set;
+
+import ua.nure.ostpc.malibu.shedule.dao.DAOFactory;
+import ua.nure.ostpc.malibu.shedule.dao.ScheduleDAO;
+import ua.nure.ostpc.malibu.shedule.entity.Period;
+import ua.nure.ostpc.malibu.shedule.entity.Schedule;
+
 public class Demo {
 
-	public static void main(String[] args) {
-	}
+	public static void main(String[] args) throws SQLException, ParseException {
+		/*
+		 * DAOFactory df = DAOFactory.getDAOFactory(DAOFactory.MSSQL);
+		 * ScheduleDAO scheduleDAO = df.getScheduleDAO(); SimpleDateFormat sdf =
+		 * new SimpleDateFormat("dd-MM-yyyy"); Date date = new
+		 * Date(sdf.parse("17-06-2014").getTime()); Period period =
+		 * scheduleDAO.readPeriod(date); System.out.println(period);
+		 */
 
+		/*
+		 * DAOFactory df = DAOFactory.getDAOFactory(DAOFactory.MSSQL);
+		 * ScheduleDAO scheduleDAO = df.getScheduleDAO(); SimpleDateFormat sdf =
+		 * new SimpleDateFormat("dd-MM-yyyy"); Date date = new
+		 * Date(sdf.parse("13-06-2014").getTime()); Period period =
+		 * scheduleDAO.readPeriod(date); System.out.println(period); Schedule
+		 * schedule = scheduleDAO.readSchedule(period);
+		 * System.out.println(schedule);
+		 */
+
+		DAOFactory df = DAOFactory.getDAOFactory(DAOFactory.MSSQL);
+		ScheduleDAO scheduleDAO = df.getScheduleDAO();
+		SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+		Date startDate = new Date(sdf.parse("01-06-2014").getTime());
+		Date endDate = new Date(sdf.parse("30-06-2014").getTime());
+		Set<Schedule> schedules = scheduleDAO.readSchedules(startDate, endDate);
+		Iterator<Schedule> it = schedules.iterator();
+		while (it.hasNext()) {
+			Schedule schedule = it.next();
+			System.out.println(schedule);
+		}
+	}
 }
--- FitnessUA/src/ua/nure/ostpc/malibu/shedule/dao/mssql/MSsqlDAOFactory.java
+++ FitnessUA/src/ua/nure/ostpc/malibu/shedule/dao/mssql/MSsqlDAOFactory.java
@@ -1,6 +1,7 @@
 package ua.nure.ostpc.malibu.shedule.dao.mssql;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 
 import javax.naming.Context;
@@ -18,10 +19,10 @@ import ua.nure.ostpc.malibu.shedule.dao.ScheduleDAO;
 
 public class MSsqlDAOFactory extends DAOFactory {
 	private static final Logger log = Logger.getLogger(MSsqlDAOFactory.class);
-	
+
 	@SuppressWarnings("unused")
 	private static final String DRIVER = "com.microsoft.jdbc.sqlserver.SQLServerDriver";
-	private static final String DB_URL = "jdbc:microsoft:sqlserver://localhost:1433, userName, password";
+	private static final String DB_URL = "jdbc:sqlserver://localhost;instanceName=TEMPUS;database=FitnessUA;integratedSecurity=true;";
 
 	public MSsqlDAOFactory() {
 	}
@@ -32,68 +33,53 @@ public class MSsqlDAOFactory extends DAOFactory {
 	 * @return
 	 * @throws SQLException
 	 */
-	protected static synchronized Connection getConnection() throws SQLException {
+	protected static synchronized Connection getConnection()
+			throws SQLException {
 		Connection con = null;
 		try {
-			Context initContext = new InitialContext();
-			Context envContext  = (Context)initContext.lookup("java:/comp/env");
-			
-			// ST4DB - the name of data source
-			DataSource dataSource = (DataSource)envContext.lookup("jdbc/ST4DB");
-			con = dataSource.getConnection();
-		} catch (NamingException ex) {
-			log.error("Cannot obtain a connection from the pool", ex);			
+			con = DriverManager.getConnection(DB_URL);
+			con.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+			con.setAutoCommit(false);
+		} catch (SQLException e) {
+			System.err.println(e.getMessage());
 		}
-//		return con;
-//		if (con == null)
-//			try {
-//				con = DriverManager.getConnection(DB_URL);
-//				con.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-//				con.setAutoCommit(false);
-//			} catch (SQLException e) {
-//				System.err.println(e.getMessage());
-//			}
 		return con;
+		/*
+		 * Connection con = null; try { Context initContext = new
+		 * InitialContext(); Context envContext =
+		 * (Context)initContext.lookup("java:/comp/env");
+		 * 
+		 * // ST4DB - the name of data source DataSource dataSource =
+		 * (DataSource)envContext.lookup("jdbc/ST4DB"); con =
+		 * dataSource.getConnection(); } catch (NamingException ex) {
+		 * log.error("Cannot obtain a connection from the pool", ex); }
+		 */
+		// return con;
+		// if (con == null)
+		// try {
+		// con = DriverManager.getConnection(DB_URL);
+		// con.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
+		// con.setAutoCommit(false);
+		// } catch (SQLException e) {
+		// System.err.println(e.getMessage());
+		// }
+		/*return con;*/
 	}
 
-/*	protected void commit(Connection con) {
-		try {
-			con.commit();
-		} catch (SQLException e) {
-			try {
-				con.rollback();
-			} catch (SQLException e1) {
-				e1.printStackTrace();
-				System.err.println(e1.getMessage());
-			}
-			System.err.println(e.getMessage());
-		}
-	}
-
-	protected void close(Connection con) {
-		try {
-			con.close();
-			con = null;
-		} catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-	}
-
-	protected void commitAndClose(Connection con) {
-		try {
-			con.commit();
-		} catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-		try {
-			con.close();
-			con = null;
-		} catch (SQLException e) {
-			System.err.println(e.getMessage());
-		}
-	}
-
-*/	public EmployeeDAO getEmployeeDAO() {
+	/*
+	 * protected void commit(Connection con) { try { con.commit(); } catch
+	 * (SQLException e) { try { con.rollback(); } catch (SQLException e1) {
+	 * e1.printStackTrace(); System.err.println(e1.getMessage()); }
+	 * System.err.println(e.getMessage()); } }
+	 * 
+	 * protected void close(Connection con) { try { con.close(); con = null; }
+	 * catch (SQLException e) { System.err.println(e.getMessage()); } }
+	 * 
+	 * protected void commitAndClose(Connection con) { try { con.commit(); }
+	 * catch (SQLException e) { System.err.println(e.getMessage()); } try {
+	 * con.close(); con = null; } catch (SQLException e) {
+	 * System.err.println(e.getMessage()); } }
+	 */public EmployeeDAO getEmployeeDAO() {
 		return new MSsqlEmployeeDAO();
 	}
 
