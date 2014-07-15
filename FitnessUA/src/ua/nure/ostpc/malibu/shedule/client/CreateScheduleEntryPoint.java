5a969245b6c1f2940aa64e03464e286dcf1ecdee
Volodymyr Semerkov <semerkoff@gmail.com>
2014-07-16 00:23:20 +0300

    Empty test CreateSchedule module added.

--- FitnessUA/src/ua/nure/ostpc/malibu/shedule/client/CreateScheduleEntryPoint.java
+++ FitnessUA/src/ua/nure/ostpc/malibu/shedule/client/CreateScheduleEntryPoint.java
@@ -1,9 +1,5 @@
 package ua.nure.ostpc.malibu.shedule.client;
 
-//import com.claudiushauptmann.gwt.multipage.client.MultipageEntryPoint;
-
-import ua.nure.ostpc.malibu.shedule.shared.FieldVerifier;
-
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.event.dom.client.ClickEvent;
@@ -20,43 +16,28 @@ import com.google.gwt.user.client.ui.RootPanel;
 import com.google.gwt.user.client.ui.TextBox;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-
-
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
  */
-public class FitnessUA implements EntryPoint {
-	/**
-	 * The message displayed to the user when the server cannot be reached or
-	 * returns an error.
-	 */
-	private static final String SERVER_ERROR = "An error occurred while "
-			+ "attempting to contact the server. Please check your network "
-			+ "connection and try again.";
-
-	/**
-	 * Create a remote service proxy to talk to the server-side Greeting service.
-	 */
-	private final GreetingServiceAsync greetingService = GWT
-			.create(GreetingService.class);
+public class CreateScheduleEntryPoint implements EntryPoint {
+	private final CreateScheduleServiceAsync createScheduleService = GWT
+			.create(CreateScheduleService.class);
 
 	/**
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-		final Button sendButton = new Button("Send");
+		final Button sendButton = new Button("Create");
 		final TextBox nameField = new TextBox();
-		nameField.setText("GWT User");
-		final Label errorLabel = new Label();
+		nameField.setText("Schedule");
 
 		// We can add style names to widgets
 		sendButton.addStyleName("sendButton");
 
 		// Add the nameField and sendButton to the RootPanel
 		// Use RootPanel.get() to get the entire body element
-		RootPanel.get("nameFieldContainer").add(nameField);
+		RootPanel.get("scheduleContainer").add(nameField);
 		RootPanel.get("sendButtonContainer").add(sendButton);
-		RootPanel.get("errorLabelContainer").add(errorLabel);
 
 		// Focus the cursor on the name field when the app loads
 		nameField.setFocus(true);
@@ -112,19 +93,13 @@ public class FitnessUA implements EntryPoint {
 			 * Send the name from the nameField to the server and wait for a response.
 			 */
 			private void sendNameToServer() {
-				// First, we validate the input.
-				errorLabel.setText("");
 				String textToServer = nameField.getText();
-				if (!FieldVerifier.isValidName(textToServer)) {
-					errorLabel.setText("Please enter at least four characters");
-					return;
-				}
 
 				// Then, we send the input to the server.
 				sendButton.setEnabled(false);
 				textToServerLabel.setText(textToServer);
 				serverResponseLabel.setText("");
-				greetingService.greetServer(textToServer,
+				createScheduleService.createSchedule(textToServer,
 						new AsyncCallback<String>() {
 							public void onFailure(Throwable caught) {
 								// Show the RPC error message to the user
@@ -132,7 +107,7 @@ public class FitnessUA implements EntryPoint {
 										.setText("Remote Procedure Call - Failure");
 								serverResponseLabel
 										.addStyleName("serverResponseLabelError");
-								serverResponseLabel.setHTML(SERVER_ERROR);
+								serverResponseLabel.setHTML("error");
 								dialogBox.center();
 								closeButton.setFocus(true);
 							}
