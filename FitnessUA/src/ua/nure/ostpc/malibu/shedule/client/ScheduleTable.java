e2d92de419fab33082c61bcc5caf4def2e4eae68
Volodymyr Semerkov <semerkoff@gmail.com>
2014-08-04 23:39:15 +0300

    Draw workspace method.

--- FitnessUA/src/ua/nure/ostpc/malibu/shedule/client/ScheduleTable.java
+++ FitnessUA/src/ua/nure/ostpc/malibu/shedule/client/ScheduleTable.java
@@ -9,7 +9,10 @@ import java.util.Map;
 import ua.nure.ostpc.malibu.shedule.entity.Club;
 import ua.nure.ostpc.malibu.shedule.entity.Employee;
 
+import com.google.gwt.event.dom.client.ChangeEvent;
+import com.google.gwt.event.dom.client.ChangeHandler;
 import com.google.gwt.i18n.client.DateTimeFormat;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.AbsolutePanel;
 import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.Label;
@@ -32,6 +35,9 @@ public class ScheduleTable extends FlexTable {
 
 	private Date startDate;
 	private Date endDate;
+	private Map<Long, SelectItem> clubPrefSelectItems;
+	private Map<Long, ListBox> shiftListBoxes;
+	private Map<Long, ListBox> empOnShiftListBoxes;
 
 	static {
 		dayOfWeekMap = new HashMap<String, String>();
@@ -69,24 +75,46 @@ public class ScheduleTable extends FlexTable {
 			Map<Long, List<Employee>> employeesByClubs) {
 		Date startDate = new Date(currentDate.getTime());
 		Date endDate = new Date(currentDate.getTime());
-		CalendarUtil.addDaysToDate(currentDate, daysInTable);
 		CalendarUtil.addDaysToDate(endDate, daysInTable - 1);
 		ScheduleTable scheduleTable = new ScheduleTable(startDate, endDate);
 		scheduleTable.setWidth("1040px");
 		scheduleTable.setBorderWidth(1);
+		scheduleTable.drawTimeLine();
+		scheduleTable.drawClubColumn(dependentClubs, employeesByClubs);
+		scheduleTable.drawWorkSpace(dependentClubs);
+		return scheduleTable;
+	}
 
-		scheduleTable.getColumnFormatter().setStyleName(0, "clubColumn");
-		scheduleTable.insertRow(0);
-		scheduleTable.insertCell(0, 0);
-		scheduleTable.setText(0, 0, "Day of week");
-		scheduleTable.insertRow(1);
-		scheduleTable.insertCell(1, 0);
-		scheduleTable.setText(1, 0, "Date");
+	private void drawTimeLine() {
+		getColumnFormatter().setStyleName(0, "clubColumn");
+		insertRow(0);
+		insertCell(0, 0);
+		setText(0, 0, "Day of week");
+		insertRow(1);
+		insertCell(1, 0);
+		setText(1, 0, "Date");
+		Date currentDate = new Date(startDate.getTime());
+		int headColunm = 1;
+		while (currentDate.getTime() <= endDate.getTime()) {
+			insertCell(0, headColunm);
+			insertCell(1, headColunm);
+			setText(0, headColunm,
+					dayOfWeekMap.get(dayOfWeekFormat.format(currentDate)));
+			setText(1, headColunm, tableDateFormat.format(currentDate));
+			headColunm++;
+			CalendarUtil.addDaysToDate(currentDate, 1);
+		}
+	}
 
+	private void drawClubColumn(List<Club> dependentClubs,
+			Map<Long, List<Employee>> employeesByClubs) {
 		int rowNumber = 2;
+		clubPrefSelectItems = new LinkedHashMap<Long, SelectItem>();
+		shiftListBoxes = new LinkedHashMap<Long, ListBox>();
+		empOnShiftListBoxes = new LinkedHashMap<Long, ListBox>();
 		for (Club club : dependentClubs) {
-			scheduleTable.insertRow(rowNumber);
-			scheduleTable.insertCell(rowNumber, 0);
+			insertRow(rowNumber);
+			insertCell(rowNumber, 0);
 
 			AbsolutePanel clubTotalPanel = new AbsolutePanel();
 			clubTotalPanel.setWidth("350px");
@@ -104,11 +132,11 @@ public class ScheduleTable extends FlexTable {
 			DynamicForm employeesInClubForm = new DynamicForm();
 			employeesInClubForm.setStyleName("selectItem");
 
-			SelectItem clubPrefsSelectItem = new SelectItem();
-			clubPrefsSelectItem.setTextBoxStyle("item");
-			clubPrefsSelectItem.setMultiple(true);
-			clubPrefsSelectItem.setTitle("");
-			clubPrefsSelectItem
+			SelectItem clubPrefSelectItem = new SelectItem();
+			clubPrefSelectItem.setTextBoxStyle("item");
+			clubPrefSelectItem.setMultiple(true);
+			clubPrefSelectItem.setTitle("");
+			clubPrefSelectItem
 					.setMultipleAppearance(MultipleAppearance.PICKLIST);
 
 			List<Employee> employees = employeesByClubs.get(club.getClubId());
@@ -117,8 +145,9 @@ public class ScheduleTable extends FlexTable {
 				map.put(String.valueOf(employee.getEmployeeId()),
 						employee.getNameForSchedule());
 			}
-			clubPrefsSelectItem.setValueMap(map);
-			employeesInClubForm.setItems(clubPrefsSelectItem);
+			clubPrefSelectItem.setValueMap(map);
+			employeesInClubForm.setItems(clubPrefSelectItem);
+			clubPrefSelectItems.put(club.getClubId(), clubPrefSelectItem);
 			clubPanel.add(employeesInClubForm, 0, 30);
 
 			clubTotalPanel.add(clubPanel, 0, 0);
@@ -133,12 +162,24 @@ public class ScheduleTable extends FlexTable {
 			shiftsNumberLabel.setStyleName("smallLabel");
 			clubShiftPanel.add(shiftsNumberLabel, 5, 2);
 
-			ListBox shiftsNumberListBox = new ListBox(false);
+			ListBox shiftListBox = new ListBox(false);
 			for (int i = 1; i <= 24; i++) {
-				shiftsNumberListBox.addItem(String.valueOf(i));
+				shiftListBox.addItem(String.valueOf(i));
 			}
-			shiftsNumberListBox.setSelectedIndex(0);
-			clubShiftPanel.add(shiftsNumberListBox, 16, 30);
+			shiftListBox.setSelectedIndex(0);
+
+			shiftListBox.getElement().setId(String.valueOf(club.getClubId()));
+			shiftListBox.addChangeHandler(new ChangeHandler() {
+
+				@Override
+				public void onChange(ChangeEvent event) {
+					ListBox listBox = (ListBox) event.getSource();
+					Window.alert(listBox.getElement().getId());
+				}
+			});
+
+			clubShiftPanel.add(shiftListBox, 16, 30);
+			shiftListBoxes.put(club.getClubId(), shiftListBox);
 			clubTotalPanel.add(clubShiftPanel, 190, 0);
 
 			AbsolutePanel clubEmpPanel = new AbsolutePanel();
@@ -151,30 +192,31 @@ public class ScheduleTable extends FlexTable {
 			clubEmpLabel.setStyleName("smallLabel");
 			clubEmpPanel.add(clubEmpLabel, 5, 2);
 
-			ListBox clubEmpListBox = new ListBox(false);
+			ListBox empOnShiftListBox = new ListBox(false);
 			for (int i = 1; i <= 20; i++) {
-				clubEmpListBox.addItem(String.valueOf(i));
+				empOnShiftListBox.addItem(String.valueOf(i));
 			}
-			clubEmpListBox.setSelectedIndex(0);
-			clubEmpPanel.add(clubEmpListBox, 16, 30);
+			empOnShiftListBox.setSelectedIndex(0);
+			clubEmpPanel.add(empOnShiftListBox, 16, 30);
+			empOnShiftListBoxes.put(club.getClubId(), empOnShiftListBox);
 			clubTotalPanel.add(clubEmpPanel, 270, 0);
 
-			scheduleTable.setWidget(rowNumber, 0, clubTotalPanel);
+			setWidget(rowNumber, 0, clubTotalPanel);
 			rowNumber++;
 		}
+	}
 
-		int headColunm = 1;
-
-		while (startDate.getTime() <= endDate.getTime()) {
-			scheduleTable.insertCell(0, headColunm);
-			scheduleTable.insertCell(1, headColunm);
-			scheduleTable.setText(0, headColunm,
-					dayOfWeekMap.get(dayOfWeekFormat.format(startDate)));
-			scheduleTable.setText(1, headColunm,
-					tableDateFormat.format(startDate));
-			headColunm++;
-			CalendarUtil.addDaysToDate(startDate, 1);
+	private void drawWorkSpace(List<Club> dependentClubs) {
+		int column = 1;
+		int row = 2;
+		int clubsInTable = dependentClubs.size();
+		int daysInTable = getDaysInTable();
+		int endColumn = column + daysInTable;
+		int endRow = row + clubsInTable;
+		for (int startColumn = column; startColumn < endColumn; startColumn++) {
+			for (int startRow = row; startRow < endRow; startRow++) {
+				insertCell(startRow, startColumn);
+			}
 		}
-		return scheduleTable;
 	}
 }
