c0216afd7b98f898324f6e392d1648f8050fb0d8
Volodymyr Semerkov <semerkoff@gmail.com>
2014-09-23 14:18:19 +0300

    Constants

--- FitnessUA/src/ua/nure/ostpc/malibu/shedule/parameter/AppConstants.java
+++ FitnessUA/src/ua/nure/ostpc/malibu/shedule/parameter/AppConstants.java
@@ -6,6 +6,8 @@ public interface AppConstants {
 	public static final String PASSWORD = "password";
 	public static final String PERIOD_ID = "periodId";
 	public static final String KEY = "key";
+	public static final String CATEGORY_MARKER = "c";
+	public static final String EMPLOYEE_MARKER = "e";
 	public static final String SERVER_ERROR = "An error occurred while "
 			+ "attempting to contact the server. Please check your network "
 			+ "connection and try again.";
