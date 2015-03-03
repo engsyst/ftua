8bc8dd595fc1f1ff7582d1310af8191714a4cbec
engsyst <engsyst@Gig>
2015-03-03 11:07:50 +0200

    Added View ActiveEmpWithUser contains not deleted Employee and it User

--- FitnessUA/sql/mssql/schemaAllServers.sql
+++ FitnessUA/sql/mssql/schemaAllServers.sql
@@ -822,7 +822,36 @@ AS
 
 GO
 
+/****** Object:  View [dbo].[ActiveEmpWithUser]    Script Date: 03/03/2015 10:50:11 ******/
+IF  EXISTS (SELECT * FROM sys.views WHERE object_id = OBJECT_ID(N'[dbo].[ActiveEmpWithUser]'))
+DROP VIEW [dbo].[ActiveEmpWithUser]
+GO
 
+USE [FitnessUA]
+GO
+
+/****** Object:  View [dbo].[ActiveEmpWithUser]    Script Date: 03/03/2015 10:50:11 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+
+CREATE VIEW [dbo].[ActiveEmpWithUser] WITH SCHEMABINDING
+AS
+SELECT  DISTINCT   dbo.Employee.EmployeeId, dbo.Employee.Firstname, dbo.Employee.Secondname, dbo.Employee.Lastname, dbo.Employee.Birthday, 
+                      dbo.Employee.Address, dbo.Employee.Passportint, dbo.Employee.Idint, dbo.Employee.CellPhone, dbo.Employee.WorkPhone, dbo.Employee.HomePhone, 
+                      dbo.Employee.Email, dbo.Employee.Education, dbo.Employee.Notes, dbo.Employee.PassportIssuedBy, dbo.Employee.IsDeleted, dbo.Employee.Colour, 
+                      dbo.Client.UserId, dbo.Client.Login, dbo.Client.PwdHache
+FROM         dbo.Employee LEFT JOIN
+                      dbo.EmployeeUserRole ON dbo.Employee.EmployeeId = dbo.EmployeeUserRole.EmployeeId left JOIN
+                      dbo.Client ON dbo.EmployeeUserRole.UserId = dbo.Client.UserId
+WHERE
+                      Employee.IsDeleted = 0
+
+
+GO
 
 INSERT INTO Club(Title, IsIndependent, IsDeleted) VALUES('Бавария', 0, 0);
 INSERT INTO Club(Title, IsIndependent, IsDeleted) VALUES('Маршала Жукова', 0, 0);
